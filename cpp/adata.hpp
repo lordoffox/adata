41dbf52ed35c108497d3e7330f9b35ced3e1ef87
lordoffox <lordoffox@gamil.com>
2016-08-01 09:27:45 +0800

    add size_of for std::string

--- cpp/adata.hpp
+++ cpp/adata.hpp
@@ -2474,6 +2474,14 @@ namespace adata
     value_ptr[ADATA_LEPOS8_7] = read_ptr[7];
   }
 
+  template<typename stream_ty, typename alloc_type>
+  ADATA_INLINE int32 size_of(std::basic_string<char, std::char_traits<char>, alloc_type>& str)
+  {
+    int32_t len = (int32_t)str.length();
+    len += size_of(len);
+    return len;
+  }
+
   template<typename stream_ty , typename alloc_type>
   ADATA_INLINE void read(stream_ty& stream, std::basic_string<char, std::char_traits<char>, alloc_type>& str)
   {
