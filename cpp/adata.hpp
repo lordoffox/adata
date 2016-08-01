435a07f9bc3f47a8ed99bee4bf741782b48445e2
lordoffox <lordoffox@gamil.com>
2016-08-01 11:19:31 +0800

    remove duplicate const for write string

--- cpp/adata.hpp
+++ cpp/adata.hpp
@@ -2492,7 +2492,7 @@ namespace adata
   }
 
   template<typename stream_ty, typename alloc_type>
-  ADATA_INLINE void write(stream_ty& stream, const std::basic_string<char, std::char_traits<char>, alloc_type> const& str)
+  ADATA_INLINE void write(stream_ty& stream, std::basic_string<char, std::char_traits<char>, alloc_type> const& str)
   {
     int32_t len = (int32_t)str.length();
     write(stream, len);
