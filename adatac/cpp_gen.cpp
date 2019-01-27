5f3f647ec7308aa57b11befb8e3d2a9eada75d6e
Evan <lordoffox@gmail.com>
2019-01-27 14:37:41 +0800

    set default value if is null

--- adatac/cpp_gen.cpp
+++ adatac/cpp_gen.cpp
@@ -425,6 +425,12 @@ namespace cpp_gen
         var_name += member.m_name;
         os << tabs(2) << "else {" << var_name << ".clear();}" << std::endl;
       }
+      else if (member.is_initable())
+      {
+        std::string var_name = "value.";
+        var_name += member.m_name;
+        os << tabs(2) << "else {" << var_name << " = " << make_type_default(desc_define, member) << ";}" << std::endl;
+      }
       total_mask |= tag_mask;
       tag_mask <<= 1;
     }
